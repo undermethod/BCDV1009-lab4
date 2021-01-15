d711c3fa5497797656acb9a68b15e468c3c6028c
Muhamed Hasanen <muhammed.omara@gmail.com>
2021-01-15 07:20:47 +0300

    commit on main

--- src/components/List/List.test.js
+++ src/components/List/List.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import List from "./List";
-import DATA from "../../../mockData";
+import DATA from "../../mockData";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  render(<List tasks={DATA} />, div);
+describe("<List /> tests", () => {
+    it("renders without crashing", () => {
+        const div = document.createElement("div");
+        render(<List tasks={DATA} />, div);
+    });
+
+    it("should contain DATA", () => {
+        const div = document.createElement("div");
+        render(<List tasks={DATA} />, div);
+        expect(screen.queryByText(/Eat/)).toBeInTheDocument();
+        expect(screen.queryByText(/sleep/i)).toBeInTheDocument();
+        expect(screen.queryByText(/repeat/i)).toBeInTheDocument();
+    });
 });
