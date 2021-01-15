73d48cae96b9005ab047a8b9f7e12177af8a84b3
undermethod <undermethod@gmail.com>
2021-01-14 21:36:29 -0700

    added Add click test to App.test.js, and mockData mv

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import DATA from "../mockData";
+import DATA from "./mockData";
+import userEvent from "@testing-library/user-event";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  render(<App tasks={DATA} />, div);
+describe("<Form /> tests", () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    render(<App tasks={DATA} />, div);
+  });
+
+  it("should add properly", () => {
+    render(<App tasks={DATA} />);
+    userEvent.type(screen.getByRole("textbox"), "new thing to be done");
+    userEvent.click(screen.getByText(/Add/));
+    expect(screen.queryByText(/new thing to be done/i)).toBeInTheDocument();
+  });
 });
--- src/components/Form/Form.test.js
+++ src/components/Form/Form.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import Form from "./Form";
-import DATA from "../../../mockData";
+import DATA from "../../mockData";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  render(<Form tasks={DATA} />, div);
+describe("<Form /> tests", () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    render(<Form tasks={DATA} />, div);
+  });
 });
